e558d2d5379ae7db1034ce211db273eda27886f7
Devin Thomeczek <141187056+dthomeczek@users.noreply.github.com>
2023-10-09 00:51:55 -0500

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+//Source code
+package code;
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("\n\tThis is my first Java program:\n\t\tMy name is Devin Thomeczek\n");
+        System.out.println("\t\tWelcome to Java!\n");
+        System.out.println("\tI have the resources to run Java programs\n");
+    }
+}
